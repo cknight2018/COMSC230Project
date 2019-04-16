ed9853af0ec31ba04c71631bb60bc524fb2eb63f
Casey Knight <cknight2018@gmail.com>
2019-04-16 15:38:03 -0400

    Add files via upload
    Sign in/up screen with full functionality connected to firebase database. Screenshots of my file paths and gradle files attached.

---
+++ MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.hawkshop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this, SignIn.class);
+                startActivity(signIn);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent(MainActivity.this, SignUp.class);
+                startActivity(signUp);
+            }
+        });
+    }
+}
---
+++ SignUp.java
@@ -0,0 +1,69 @@
+package com.example.hawkshop;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignUp extends AppCompatActivity {
+
+
+    EditText editPhone, editName, editPassword;
+    Button btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        editName = findViewById(R.id.editName);
+        editPassword = findViewById(R.id.editPassword);
+        editPhone = findViewById(R.id.editPhone);
+
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
+                mDialog.setMessage("Please wait...");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.child(editPhone.getText().toString()).exists()){
+                            mDialog.dismiss();
+                            Toast.makeText(SignUp.this, "Phone Number already Registered", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            mDialog.dismiss();
+                            User user = new User(editName.getText().toString(), editPassword.getText().toString());
+                            table_user.child(editPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this, "Sign Up successful", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
